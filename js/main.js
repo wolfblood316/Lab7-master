f04fe9c511d6ec8d932257bcfcf1ebb860d51fd8
wolfblood316 <adrianlandin@yahoo.com>
2014-12-01 21:31:37 -0500

    lab 7

---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+/*
+Element = tag  = node
+
+
+Element - type 1;
+ATTRIBUTE  - type 2
+TEXT - type 3
+
+
+document.getElementById('main')
+document.querySelector('.active');
+document.querySelectorAll('.active');
+
+
+
+var columns = document.querySelectorAll('.columns');
+
+for(var item in columns){
+	columns[item].onclick = function(){
+		this.style.display = "none";
+	}
+}
+*/
+$(document).ready(function(){
+	
+	$('.toggle').click(function(){
+		$(this).next().fadeToggle('fast');
+		$(this).next().text("hello There");
+ 	});
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
